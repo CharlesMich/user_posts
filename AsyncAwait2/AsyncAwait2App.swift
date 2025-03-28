8287fe00b8d37d72308610dc62e07cf3a24c42a3
CharlesMich <charleedongo@hotmail.com>
2025-03-28 11:40:04 -0500

    Initial Commit

---
+++ AsyncAwait2/AsyncAwait2App.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwait2App.swift
+//  AsyncAwait2
+//
+//  Created by Charles Michael on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwait2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
